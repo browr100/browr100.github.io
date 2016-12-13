315bf6b1e1c2edccef8ff1a6f3d65c828ea481f3
Rachel Brownjohn <browr100@newschool.edu>
2016-12-13 21:56:59 +0000

    index.html

---
+++ Catalogue/js/catalogue.js
@@ -0,0 +1,23 @@
+$(".myCheckbox1").click(function() {
+  $(".comp1").toggle();
+});
+
+$(".myCheckbox2").click(function() {
+  $(".comp2").toggle();
+});
+
+$(".myCheckbox3").click(function() {
+  $(".comp3").toggle();
+});
+
+$(".myCheckbox4").click(function() {
+  $(".comp4").toggle();
+});
+
+$(".myCheckbox5").click(function() {
+  $(".comp5").toggle();
+});
+
+$(".myCheckbox6").click(function() {
+  $(".comp6").toggle();
+});
